57cf87339d4c48bcba771337909cabb37717c9aa
Tiffany Bennett <tiffany@stormbit.net>
2015-01-25 00:46:04 -0500

    Remove .dir-locals.el

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-((nil . ((c-basic-offset . 4)
-         (company-clang-arguments . ("-std=c++11 "
-                                     "-I/home/tiffany/projects/bot/src"
-                                     "-I/home/tiffany/projects/bot/LuaBridge/Source"
-         (company-c-headers-path-user . ("/home/tiffany/projects/bot/src"
-                                         "/home/tiffany/projects/bot/LuaBridge/Source"))
-         (flycheck-clang-include-path . ("/home/tiffany/projects/bot/src"
-                                         "/home/tiffany/projects/bot/LuaBridge/Source")))))))
