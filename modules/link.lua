4d4934fcb8287971f82c63a97fd9d91cc92145aa
Tiffany Bennett <tiffany@stormbit.net>
2015-02-08 23:20:26 -0500

    PNG metadata

--- modules/link.lua
+++ modules/link.lua
@@ -43,9 +43,47 @@ function title_parser()
             return false, "Couldn't find <title>"
          end
          t.fn = t.fn(s:sub(i,i))
-         if t.fn == false then return t.title end
+         if t.fn == false and (not t.title or t.title == '') then
+            return 'No title'
+         end
+         if t.fn == false then return 'Title: '..t.title end
          if not t.fn then return false, "fn returned nil" end
       end
+   end
+end
+
+function png_parser()
+   local s = ""
+   local function read_word(s, o)
+      local b1,b2,b3,b4 = string.byte(s, o, o+4)
+      return bit32.bor(bit32.bor(bit32.lshift(b1, 24), bit32.lshift(b2, 16)),
+                       bit32.bor(bit32.lshift(b3, 8), b4))
+   end
+   return function(buf)
+      s = s..buf
+      if #s < 32 then
+         -- wait for more data
+         return
+      end
+      if s:sub(1, 8) ~= "\x89PNG\r\n\x1A\n" then
+         print(s:byte(1,8))
+         return false, "Malformed PNG: Missing/corrupt header"
+      end
+      local len = read_word(s, 9)
+      if s:sub(13, 16) ~= "IHDR" then
+         return false, "Malformed PNG: IHDR is not first chunk"
+      end
+      local width = read_word(s, 17)
+      local height = read_word(s, 21)
+      local bd, ct, cm, fm, im = s:byte(25, 30)
+      local color_types = {
+         [0] = 'greyscale',
+         [2] = 'RGB',
+         [3] = 'Palette',
+         [4] = 'grey+alpha',
+         [6] = 'RGBA'
+      }
+      return string.format("PNG: %dx%d@%d %s", width, height, bd, color_types[ct])
    end
 end
 
@@ -63,6 +101,8 @@ function do_request(url, cb)
          end
          if ct:sub(1, 9) == 'text/html' then
             handler = title_parser()
+         elseif ct:sub(1, 10) == 'image/png' then
+            handler = png_parser()
          else
             print("unknown content type: ", headers['Content-Type'])
             self:cancel()
@@ -71,7 +111,7 @@ function do_request(url, cb)
       unbuffered = function(self, s)
          local res, err = handler(s)
          if res then
-            cb("Title: "..res)
+            cb(res)
          elseif res == false then
             cb(err)
          end
