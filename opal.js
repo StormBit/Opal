e63c0a78ce893fd499f9792e9a65ae942bd1033d
Tiffany Bennett <tiffany@stormbit.net>
2015-10-27 20:19:32 -0400

    Modules map

--- opal.js
+++ opal.js
@@ -3,7 +3,9 @@
 const irc = require('irc');
 const Module = require('./module');
 
-let link = new Module('link');
+let modules = new Map([
+    ['link', new Module('link')]
+]);
 
 function target(from, to) {
     if (to === 'Opal') {
@@ -19,14 +21,21 @@ let client = new irc.Client('irc.stormbit.net', 'Opal', {
 
 client.addListener('message', function(from, to, message) {
     console.log('[' + to + '] <' + from + '> ' + message);
-    if (message.startsWith('+reload link')) {
-        link.reload();
-        client.notice(target(from, to), 'Reloading link');
+    if (message.startsWith('+reload ')) {
+        let name = message.slice(8);
+        if (modules.get(name)) {
+            client.notice(target(from, to), 'Reloading '+name);
+            modules.get(name).reload();
+        } else {
+            client.notice(target(from, to), 'No such module '+name);
+        }
     }
-    link.call('onMessage', [from, to, message]).then((m) => {
-        client.notice(target(from, to), m);
-    }).catch((e) => {
-        client.notice(target(from, to), er);
-        console.log(er.stack);
-    });
+    for (let mod of modules.values()) {
+        mod.call('onMessage', [from, to, message]).then((m) => {
+            client.notice(target(from, to), m);
+        }).catch((e) => {
+            client.notice(target(from, to), er);
+            console.log(er.stack);
+        });
+    }
 });
