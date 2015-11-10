f49215d9cd3a7c3e14f8488687b7b00d95acd83a
Tiffany Bennett <tiffany@stormbit.net>
2015-11-09 21:14:46 -0500

    Fix reloading

--- Module.js
+++ Module.js
@@ -19,10 +19,53 @@ export default class Module {
   stop() {
     console.log(`Stopping ${this.name}`);
     this.process.kill();
+    for (const p of this.promises) {
+      p.reject('Module reloaded');
+    }
   }
 
   start() {
     console.log(`Starting ${this.name}`);
+    this.commands = new Map();
+    this.regexes = [];
+    this.promises = new Map();
+    this.next_id = 0;
     this.process = fork(require.resolve(`./${this.name}`));
+
+    this.process.on('message', (msg) => {
+      if (msg.type === 'addCommand') {
+        this.commands.set(msg.name, {
+          func: msg.func
+        });
+      }
+      if (msg.type === 'addRegex') {
+        this.regexes.push({
+          regex: new RegExp(msg.regex, msg.flags),
+          func: msg.func
+        });
+      }
+      if (msg.type === 'reply') {
+        let entry = this.promises.get(msg.id);
+        if (!entry) {
+          console.log(`Reply for invalid callback ${msg.id}`);
+          return;
+        }
+        entry[msg.which].apply(null, msg.args);
+        this.promises.delete(msg.id);
+      }
+    });
+  }
+
+  callback(func, args) {
+    const id = this.next_id; this.next_id += 1;
+    return new Promise((resolve, reject) => {
+      this.promises.set(id, {resolve, reject});
+      this.process.send({
+        type: 'callback',
+        reply: id,
+        func,
+        args
+      });
+    });
   }
 }
--- opal.js
+++ opal.js
@@ -5,10 +5,6 @@ import Module from './Module';
 import config from './config';
 
 const modules = new Map();
-const commands = new Map();
-const regexes = [];
-const promises = new Map();
-let next_id = 0;
 
 function target(from, to) {
   if (to === 'Opal') {
@@ -20,45 +16,7 @@ function target(from, to) {
 
 function load(name) {
   const mod = new Module(name);
-  mod.process.on('message', (msg) => {
-    console.log(msg);
-    if (msg.type === 'addCommand') {
-      commands.set(msg.name, {
-        mod,
-        func: msg.func
-      });
-    }
-    if (msg.type === 'addRegex') {
-      regexes.push({
-        regex: new RegExp(msg.regex, msg.flags),
-        mod,
-        func: msg.func
-      });
-    }
-    if (msg.type === 'reply') {
-      let entry = promises.get(msg.id);
-      if (!entry) {
-        console.log(`Reply for invalid callback ${msg.id}`);
-        return;
-      }
-      entry[msg.which].apply(null, msg.args);
-      promises.delete(msg.id);
-    }
-  });
   modules.set(name, mod);
-}
-
-function callback(mod, func, args) {
-  const id = next_id; next_id += 1;
-  return new Promise((resolve, reject) => {
-    promises.set(id, {resolve, reject});
-    mod.process.send({
-      type: 'callback',
-      reply: id,
-      func,
-      args
-    });
-  });
 }
 
 for (const name of config.modules) {
@@ -78,10 +36,13 @@ for (const net of config.networks) {
   client.addListener('message', function(from, to, message) {
     console.log(`[${to}] <${from}> ${message}`);
 
-    const wrapped_callback = (mod, func, args) => {
-      callback(mod, func, args)
-        .then((m) => client.notice(target(from, to), m))
-        .catch((e) => client.notice(target(from, to), e));
+    const notice = (msg) => {
+      client.notice(target(from, to), msg);
+    }
+    const callback = (mod, func, args) => {
+      mod.callback(func, args)
+        .then((m) => notice(m))
+        .catch((e) => notice(e));
     }
 
     let command_string;
@@ -100,16 +61,16 @@ for (const net of config.networks) {
         const mod = modules.get(name);
         if (mod) {
           mod.reload();
-          client.notice(target(from, to), `Reloaded "${name}"`);
+          notice(`Reloaded "${name}"`);
         } else {
-          client.notice(target(from, to), `No such module "${name}"`);
+          notice(`No such module "${name}"`);
         }
         return;
       }
       if (command === 'load') {
         const name = args[0];
         load(name);
-        client.notice(target(from, to), `Loaded "${name}"`);
+        notice(`Loaded "${name}"`);
       }
       if (command === 'unload') {
         const name = args[0];
@@ -117,27 +78,34 @@ for (const net of config.networks) {
         if (mod) {
           mod.stop();
           modules.delete(name);
-          client.notice(target(from, to), `Unloaded "${name}"`);
+          notice(`Unloaded "${name}"`);
         } else {
-          client.notice(target(from, to), `No such module "${name}"`);
+          notice(`No such module "${name}"`);
         }
       }
-      const entry = commands.get(command);
-      if (entry) {
-        wrapped_callback(entry.mod, entry.func, [from, to, args]);
-      } else {
-        client.notice(target(from, to), `No such command "${command}"`);
+      let found = false;
+      for (const mod of modules.values()) {
+        const entry = mod.commands.get(command);
+        if (entry) {
+          callback(mod, entry.func, [from, to, args]);
+          found = true;
+        }
+      }
+      if (!found) {
+        notice(`No such command "${command}"`);
       }
       return;
     }
 
-    for (const entry of regexes) {
-      const result = message.match(entry.regex);
-      if (!result) {
-        continue;
-      }
-      for (const match of result) {
-        wrapped_callback(entry.mod, entry.func, [from, to, match]);
+    for (const mod of modules.values()) {
+      for (const entry of mod.regexes) {
+        const result = message.match(entry.regex);
+        if (!result) {
+          continue;
+        }
+        for (const match of result) {
+          callback(mod, entry.func, [from, to, match]);
+        }
       }
     }
   });
